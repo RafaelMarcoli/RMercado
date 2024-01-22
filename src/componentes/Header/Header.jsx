a58d4426dffd0130284faac9fa736fae85c1132d
Rafaelm <isaizanami@gmail.com>
2024-01-22 10:37:38 -0300

    colocado transition no hover dos botoes.

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -16,23 +16,23 @@ function Header() {
     <header className="header">
         
         
-          <Link to="/">
-            <img className="logo" src="./src/assets/logo/R.Mercado.png" alt="logo" />
-           </Link>
+      <Link to="/">
+         <img className="logo" src="./src/assets/logo/R.Mercado.png" alt="logo" />
+      </Link>
         
         
         <div className="nomesheader">
-        <AiOutlineDoubleRight className="icon" size={27}/>
-        <AiOutlineDoubleRight className="icon1" size={27}/>
-        <AiOutlineDoubleRight className="icon" size={27}/>
-        <h1>R.M<span>ercado</span></h1>
+          <AiOutlineDoubleRight className="icon" size={27}/>
+          <AiOutlineDoubleRight className="icon1" size={27}/>
+          <AiOutlineDoubleRight className="icon" size={27}/>
+          <h1>R.M<span>ercado</span></h1>
           <AiOutlineDoubleLeft className="icon" size={27}/>
           <AiOutlineDoubleLeft className="icon1" size={27}/>
           <AiOutlineDoubleLeft className="icon" size={27}/>
         </div>
 
         <div >
-            <Menu />
+          <Menu />
         </div>
                   
          
