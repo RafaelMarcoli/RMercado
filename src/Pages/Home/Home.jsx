08836deb41ea5bd8406e1a7e9469443ba32385a6
Rafaelm <isaizanami@gmail.com>
2024-02-07 00:21:42 -0300

    adicionando banner a pagina home.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,14 @@ import Header from '../../componentes/Header/Header';
 import Footer from '../../componentes/Footer/Footer';
 import Conteiner from '../../componentes/Conteiner/Conteiner';
 import Category from '../../componentes/Category/Category';
-import stylus from './Home.module.css'
+import styles from './Home.module.css'
 
 function Home() {
   return (
-    <main className={stylus.home}>
+    <main className={styles.home}>
       <Header />
       <Conteiner>
-
+        
         <Category category1='alimentos' titulo='Alimentos' /> 
         <Category category1='bebidas' titulo='Bebidas' /> 
         <Category category1='limpeza' titulo='Limpeza' /> 
---
+++ src/componentes/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+import styles from './Banner.module.css'
+
+function Banner() {
+  return (
+    <div className={styles.banner_a}></div>
+  )
+}
+
+export default Banner
