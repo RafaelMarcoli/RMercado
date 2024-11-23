6de5d38855f08fecb7a13436728896f4bda5dd76
Rafaelm <isaizanami@gmail.com>
2024-11-23 17:27:57 -0300

    .

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -10,22 +10,22 @@ function Header() {
     <header className="header">
 
       <Link to="/">
-         <img className="logo" src="./src/assets/logo/R.Mercado.png" alt="logo" />
+        <img className="logo" src="/assets/logo/R.Mercado.png" alt="logo" />
       </Link>
-        
-        <div className="nomesheader">
-          <AiOutlineDoubleRight className="icon" size={27}/>
-          <AiOutlineDoubleRight className="icon1" size={27}/>
-          <AiOutlineDoubleRight className="icon" size={27}/>
-          <h1>R.M<span>ercado</span></h1>
-          <AiOutlineDoubleLeft className="icon" size={27}/>
-          <AiOutlineDoubleLeft className="icon1" size={27}/>
-          <AiOutlineDoubleLeft className="icon" size={27}/>
-        </div>
 
-        <div >
-          <Menu />
-        </div>        
+      <div className="nomesheader">
+        <AiOutlineDoubleRight className="icon" size={27} />
+        <AiOutlineDoubleRight className="icon1" size={27} />
+        <AiOutlineDoubleRight className="icon" size={27} />
+        <h1>R.M<span>ercado</span></h1>
+        <AiOutlineDoubleLeft className="icon" size={27} />
+        <AiOutlineDoubleLeft className="icon1" size={27} />
+        <AiOutlineDoubleLeft className="icon" size={27} />
+      </div>
+
+      <div >
+        <Menu />
+      </div>
     </header>
   )
 }
