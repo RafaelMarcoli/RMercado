918de63f3506be4517a9ebca3ec949d0b4a233d4
Rafaelm <isaizanami@gmail.com>
2024-11-23 17:47:02 -0300

    alterando caminho da imagem no card

--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -1,18 +1,18 @@
 import styles from './Card.module.css'
 
-function Card({ imagem , nome , preco, category}) {
-  
+function Card({ imagem, nome, preco, category }) {
+
   return (
-    <div 
+    <div
       className={`${styles.card}
       ${category === "alimentos" ? styles.card_a : ''}
       ${category === "bebidas" ? styles.card_b : ''}
       ${category === "limpeza" ? styles.card_l : ''}`} id={`${imagem}`}>
-        <img className={styles.img} src={`./src/assets/${category}/${imagem}`} alt={`${nome}`} />
-        <p className={`${category === "alimentos"  ? styles.nomes_a : ''} 
-                       ${category === "bebidas"  ? styles.nomes_b : ''}
-                       ${category === "limpeza"  ? styles.nomes_l : ''}`}>{nome}</p>
-        <p className={styles.precos}><span>R$</span> {preco}</p>
+      <img className={styles.img} src={`/assets/${category}/${imagem}`} alt={`${nome}`} />
+      <p className={`${category === "alimentos" ? styles.nomes_a : ''} 
+                       ${category === "bebidas" ? styles.nomes_b : ''}
+                       ${category === "limpeza" ? styles.nomes_l : ''}`}>{nome}</p>
+      <p className={styles.precos}><span>R$</span> {preco}</p>
 
     </div>
   )
