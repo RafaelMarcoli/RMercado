6180c24e10d217096dee6460fe7602c083af5b4f
Rafaelm <isaizanami@gmail.com>
2024-11-23 17:58:07 -0300

    banner

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,29 +2,24 @@ import Header from '../../componentes/Header/Header';
 import Footer from '../../componentes/Footer/Footer';
 import Conteiner from '../../componentes/Conteiner/Conteiner';
 import Category from '../../componentes/Category/Category';
-import styles from './Home.module.css'
+import styles from './Home.module.css';
 import Banner from '../../componentes/Banner/Banner';
-import Banner_alimentos from '../../assets/img-banner/banner-alimentos.png';
-import Banner_bebidas from '../../assets/img-banner/banner-bebidas.png';
-import Banner_limpeza from '../../assets/img-banner/banner-limpeza1.png';
 
 function Home() {
   return (
     <main className={styles.home}>
       <Header />
       <Conteiner>
-        <Banner img={Banner_alimentos} />
-        <Category category1='alimentos' titulo='Alimentos' /> 
-        <Banner img={Banner_bebidas}  />
-        <Category category1='bebidas' titulo='Bebidas' /> 
-        <Banner img={Banner_limpeza}/>
-        <Category category1='limpeza' titulo='Limpeza' /> 
-
+        <Banner img="/assets/img-banner/banner-alimentos.png" /> {/* Caminho para imagem em public */}
+        <Category category1='alimentos' titulo='Alimentos' />
+        <Banner img="/assets/img-banner/banner-bebidas.png" />
+        <Category category1='bebidas' titulo='Bebidas' />
+        <Banner img="/assets/img-banner/banner-limpeza1.png" />
+        <Category category1='limpeza' titulo='Limpeza' />
       </Conteiner>
       <Footer />
     </main>
-  )
+  );
 }
 
-export default Home
-
+export default Home;
